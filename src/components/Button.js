03485dd89df96683e5cf976207b68bcb55a294ce
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-21 21:01:27 +0530

    Create context objects

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  // contextType is a reserved keyword and should be used as is
+  static contextType = LanguageContext;
+
   render() {
+    console.log(this.context);
     return <button className="ui button primary">Submit</button>;
   }
 }

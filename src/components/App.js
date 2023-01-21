b3877fbdf7296aad68f50729a4202df6305e8384
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-21 21:56:36 +0530

    Connecting Field and Button component to use language store class

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,18 @@ import React from 'react';
 import LanguageSelector from './LanguageSelector';
 import UserCreate from './UserCreate';
 import ColorContext from '../contexts/ColorContext';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
 
 class App extends React.Component {
-  state = { language: 'english' };
-
-  onLanguageChange = (language) => {
-    this.setState({ language });
-  };
-
   render() {
     return (
       <div className="ui container">
-        <LanguageSelector onLanguageChange={this.onLanguageChange} />
-        <ColorContext.Provider value="primary">
-          <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value="primary">
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     );
   }

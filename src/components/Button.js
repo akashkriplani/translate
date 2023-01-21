a41746b0b308e396d0259c5433fa9104e101b2ec
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-21 21:20:01 +0530

    Use Consumer from the context object to show the value change

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,16 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
-  // contextType is a reserved keyword and should be used as is
-  static contextType = LanguageContext;
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen';
+  }
 
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>{(value) => this.renderSubmit(value)}</LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
